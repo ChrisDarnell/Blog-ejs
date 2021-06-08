44bf38844b14cfd98aa4b9c09110a701081bd970
Chris <chrisdarnell@gmail.com>
2021-06-08 00:43:28 -0500

    New Post Template/Redirect Complete

--- app.js
+++ app.js
@@ -70,9 +70,10 @@ app.get("/posts/:postName", (req, res) => {
   posts.forEach(function (post) {
     const storedTitle = _.lowerCase(post.title);
     if (storedTitle === requestedTitle) {
-      console.log("Match Found");
-    } else{
-      console.log("No Match");
+      res.render("post", {
+          title: post.title,
+          content: post.content
+      });
     }
   });
 });
